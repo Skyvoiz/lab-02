77bc902cfe33749140c40880fd30ec93d76c64f5
Justin Cabillo <jcabillo@ualberta.ca>
2026-01-16 06:56:47 -0700

    Lab 2 commit

---
+++ app/src/main/java/com/example/listcity/MainActivity.java
@@ -0,0 +1,92 @@
+package com.example.listcity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView cityList;
+    ArrayAdapter<String> cityAdapter;
+    ArrayList<String> dataList;
+    Button addButton;
+    Button deleteButton;
+    Button confirmButton;
+    EditText userInput;
+    String userText = "";
+    String stringSelected;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cityList = findViewById(R.id.city_list);
+        addButton = findViewById((R.id.add_button));
+        deleteButton = findViewById(R.id.delete_button);
+        confirmButton = findViewById(R.id.confirm_button);
+        userInput = findViewById(R.id.user_input);
+
+        String []cities = {"Edmonton", "Vancouver", "Moscow", "Sydney", "Berlin", "Vienna", "Tokyo", "Beijing", "Osaka", "New Delhi"};
+
+        dataList = new ArrayList<>();
+        dataList.addAll(Arrays.asList(cities));
+
+        cityAdapter = new ArrayAdapter<>(this, R.layout.content, dataList);
+        cityList.setAdapter(cityAdapter);
+
+        confirmButton.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 if (!userText.isBlank()) {
+                     dataList.add(userText);
+                     cityAdapter.notifyDataSetChanged();
+                     userText = "";
+                 }
+             }
+         }
+        );
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userText = userInput.getText().toString();
+            }
+        }
+        );
+
+        cityList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                stringSelected = (String) parent.getItemAtPosition(position);
+            }
+        });
+
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 dataList.remove(stringSelected);
+                 cityAdapter.notifyDataSetChanged();
+             }
+         }
+        );
+
+        };
+
+
+
+}
